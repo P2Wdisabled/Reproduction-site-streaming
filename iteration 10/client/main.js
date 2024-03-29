6e533f2d267784ea58d7f1b2af302d3e32e8fa2d
LouisPotevin <louis.potevin@etu.unilim.fr>
2024-03-29 15:44:57 +0100

    iteration 10

---
+++ iteration 10/client/main.js
@@ -0,0 +1,83 @@
+let getmovies = async function(){
+  let option = document.querySelector('.categorie-containers').value
+  if (option == "all") {
+    // attente de la réponse à la requête demandant les données d'une collection de Lego
+    let response = await fetch("../server/script.php?action=getmovies");
+    // attente de l'extration des données en format json de la réponse à la requête
+    let data = await response.json();
+    Films.render('.films-container', data);
+  }else{
+    // attente de la réponse à la requête demandant les données d'une collection de Lego
+    let response = await fetch("../server/script.php?action=getmoviesbyid&idcategorie="+option);
+    // attente de l'extration des données en format json de la réponse à la requête
+    let data = await response.json();
+    Films.render('.films-container', data);
+  }
+}
+
+
+let showmovie = async function(idmovie){
+  let response = await fetch("../server/script.php?action=getMovie&idmovie="+idmovie);
+  // attente de l'extration des données en format json de la réponse à la requête
+  let data = await response.json();
+  Film.render('.films-container', data);
+}
+
+let addtoplaylist = async function(idmovie){
+  let idprofile = document.querySelector(".profiles-containers").value
+  await fetch("../server/script.php?action=addtoplaylist&idmovie="+idmovie+"&idprofile="+idprofile);
+  // attente de l'extration des données en format json de la réponse à la requête
+}
+
+let deletefromplaylist = async function(idmovie){
+  let idprofile = document.querySelector(".profiles-containers").value
+  await fetch("../server/script.php?action=removefromplaylist&idmovie="+idmovie+"&idprofile="+idprofile);
+  // attente de l'extration des données en format json de la réponse à la requête
+}
+
+let editnote = async function(idmovie){
+  let note = document.querySelector(".film__note").value
+  let idprofile = document.querySelector(".profiles-containers").value
+  console.log(idprofile)
+  await fetch("../server/script.php?action=editNote&idmovie="+idmovie+"&idprofile="+idprofile+"&note="+note);
+  // attente de l'extration des données en format json de la réponse à la requête
+}
+
+let getplaylist = async function(){
+  let idprofile = document.querySelector(".profiles-containers").value
+  let response = await fetch("../server/script.php?action=getplaylist&idprofile="+idprofile);
+  // attente de l'extration des données en format json de la réponse à la requête
+  let data = await response.json();
+  Playlist.render('.films-container', data);
+}
+
+let getListCategorie = async function(){
+  let response = await fetch("../server/script.php?action=getCategories");
+  let data = await response.json();
+  Categorie.render('.categorie-containers', data);
+}
+
+let getListProfil = async function(){
+  let response = await fetch("../server/script.php?action=getProfiles");
+  let data = await response.json();
+  Profiles.render('.profiles-containers', data);
+}
+
+let getPriorite = async function(){
+  let response = await fetch("../server/script.php?action=getPriorite");
+  let data = await response.json();
+  Priorite.render('.caroussel', data);
+}
+
+let getFilmbyName = async function() {
+  let idprofile = document.querySelector(".searchbar").value
+  let response = await fetch("../server/script.php?action=getMovieByName&idmovie="+idprofile);
+  let data = await response.json();
+  Films.render('.films-container', data);
+}
+
+setTimeout(() => {
+  getListCategorie();
+  getListProfil();
+  getPriorite();
+}, 500);
